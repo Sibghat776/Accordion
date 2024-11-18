66d7d62c9346f9ac5bbe2c8517d6af78d6ace967
Sibghat776 <ullahsibghat786@gmail.com>
2024-11-18 07:06:45 +0500

    On progress

---
+++ app.js
@@ -0,0 +1,13 @@
+let que1 = document.querySelector('.que1')
+let que2 = document.querySelector('.que2')
+let que3 = document.querySelector('.que3')
+let btn1 = document.querySelector('.btn1')
+let btn2 = document.querySelector('.btn2')
+let btn3 = document.querySelector('.btn3')
+let que4 = document.querySelector('.que4')
+function accordionKhula() {
+    console.log("Chala");
+    if () { } else { }
+    if () { } else { }
+    if () { } else { }
+}
